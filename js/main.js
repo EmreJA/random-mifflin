66755a56c575a39f9804fc9149f4f633b03b1e55
Emre C <f5uck@users.noreply.github.com>
2018-05-18 23:47:42 -0400

    uploaded the files

---
+++ js/main.js
@@ -0,0 +1,72 @@
+
+// Listens to the Random Mifflin button.
+document.getElementById("randomMifflin").addEventListener("click", randomMifflin);
+//Runs the random episode selector.
+function randomMifflin() {
+
+  var seasonNo = Math.ceil(Math.random() * 9);
+
+  if (seasonNo == 1) {
+      episodeNo = Math.ceil(Math.random() * 6);
+  } else if (seasonNo == 2) {
+    episodeNo = Math.ceil(Math.random() * 22);
+  } else if (seasonNo == 3) {
+    episodeNo = Math.ceil(Math.random() * 23);
+  } else if (seasonNo == 4) {
+    episodeNo = Math.ceil(Math.random() * 14);
+  } else if (seasonNo == 5) {
+    episodeNo = Math.ceil(Math.random() * 26);
+  } else if (seasonNo == 6) {
+    episodeNo = Math.ceil(Math.random() * 26);
+  } else if (seasonNo == 7) {
+    episodeNo = Math.ceil(Math.random() * 24);
+  } else if (seasonNo == 8) {
+    episodeNo = Math.ceil(Math.random() * 24);
+  } else {
+    episodeNo = Math.ceil(Math.random() * 23);
+  }
+//Writes the random episode info into the HTML.
+  document.getElementById("episodeInfo").innerHTML = 'You may want to watch Season ' + seasonNo + ', Episode ' + episodeNo + '.';
+
+  // API Stuff
+
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://api.trakt.tv/shows/the-office/seasons/' + seasonNo + '/episodes/' + episodeNo);
+
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('trakt-api-version', '2');
+  request.setRequestHeader('trakt-api-key', '[client_id]');
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders());
+      console.log('Body:', this.responseText);
+    }
+  };
+
+  request.send();
+
+}
+// Opens the filter menu.
+document.getElementById("filterToggle").addEventListener("click", filterToggle);
+function filterToggle() {
+  var x = document.getElementById('filters')
+  if (x.style.display == 'none') {
+    x.style.display = '';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+// Trying API Stuff
+
+
+
+/*
+Scott's Tots - Season 6 Episode 12
+Phyllis' Wedding - Season 3 Episode 15
+Dinner Party - Season 4 Episode 13
+The Banker - Season 6 Episode 14 (Filler Episode)
+*/
