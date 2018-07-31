24f1abac1d54f0d713b7cbb71b049b3633fc2903
Emre C <f5uck@users.noreply.github.com>
2018-07-30 21:32:00 -0400

    optimized js and deleted fontawesome

--- js/main.js
+++ js/main.js
@@ -38,21 +38,21 @@ if (seasonNo && episodeNo !== undefined) {
 
 //gets the url and splits it
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
-        }
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
     }
-    console.log('Query variable %s not found', variable);
+  }
+  console.log('Query variable %s not found', variable);
 }
 //gets the url and splits it
 
 //Runs the random episode selector.
 function randomMifflin() {
-// add loader
+  // add loader
 
   seasonNo = Math.ceil(Math.random() * 9);
   if (seasonNo == 1) {
@@ -63,15 +63,15 @@ function randomMifflin() {
     episodeNo = Math.ceil(Math.random() * 23);
     //Phyllis' Wedding - Season 3 Episode 15
     if (document.getElementById('phyllis').checked == true && episodeNo == 15) {
-       randomMifflin();
-       console.log("Phyllis' Wedding excluded. Rerunning the script.");
+      randomMifflin();
+      console.log("Phyllis' Wedding excluded. Rerunning the script.");
     }
   } else if (seasonNo == 4) {
     episodeNo = Math.ceil(Math.random() * 14);
     //Dinner Party - Season 4 Episode 13
     if (document.getElementById('jan').checked == true && episodeNo == 13) {
-       randomMifflin();
-       console.log("Dinner Party excluded. Rerunning the script.");
+      randomMifflin();
+      console.log("Dinner Party excluded. Rerunning the script.");
     };
   } else if (seasonNo == 5) {
     episodeNo = Math.ceil(Math.random() * 26);
@@ -79,8 +79,8 @@ function randomMifflin() {
     episodeNo = Math.ceil(Math.random() * 26);
     //Scott's Tots - Season 6 Episode 12
     if (document.getElementById('scott').checked == true && episodeNo == 12) {
-       randomMifflin();
-       console.log("Scott's Tots excluded. Rerunning the script.");
+      randomMifflin();
+      console.log("Scott's Tots excluded. Rerunning the script.");
     } else if (document.getElementById('scott').checked == true && episodeNo == 14) {
       randomMifflin();
       console.log("The Banker excluded. Rerunning the script.");
@@ -104,10 +104,10 @@ function randomMifflin() {
 }
 
 //gets amazon and itunes urls from db.json
-function getURL(){
+function getURL() {
   var dataRequest = new XMLHttpRequest();
-  dataRequest.open('GET','js/db.json');
-  dataRequest.onload = function (){
+  dataRequest.open('GET', 'js/db.json');
+  dataRequest.onload = () => {
     urlData = JSON.parse(dataRequest.response);
     document.getElementById('aHREF').href = urlData.season[seasonNo - 1]["0"];
     document.getElementById('iHREF').href = urlData.season[seasonNo - 1][1];
@@ -117,10 +117,10 @@ function getURL(){
 //gets amazon and itunes urls from db.json
 
 //TMDB API generates episode thumbnail
-function getThumb(){
+function getThumb() {
   var thumbRequest = new XMLHttpRequest();
   thumbRequest.open('GET', 'https://api.themoviedb.org/3/tv/2316/season/' + seasonNo + '/episode/' + episodeNo + '/images?api_key=c4e0e43db456cb63f04bead90cf06afc');
-  thumbRequest.onload = function () {
+  thumbRequest.onload = () => {
     var thumbData = JSON.parse(thumbRequest.responseText);
     var imgURL = 'https://image.tmdb.org/t/p/original/' + thumbData.stills['0'].file_path;
     document.getElementById('thumbnail').src = imgURL;
@@ -130,7 +130,7 @@ function getThumb(){
 //TMDB API generates episode thumbnail
 
 // Trakt API - Gets the information of the random episode.
-function getInfo(){
+function getInfo() {
   var request = new XMLHttpRequest();
 
   request.open('GET', 'https://api.trakt.tv/shows/the-office/seasons/' + seasonNo + '/episodes/' + episodeNo + '?extended=full');
@@ -140,24 +140,24 @@ function getInfo(){
   request.setRequestHeader('trakt-api-key', 'c66faa4cdb7c9815a82e9e066821f317ebc6086a6217a1b0934169ea55fbd492');
 
   request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        document.getElementById('title').innerHTML = response.title;
-        document.getElementById('runtime').innerHTML = response.runtime + ' mins.';
-        document.getElementById('rating').innerHTML = (response.rating).toFixed(1) + '/10';
-        document.getElementById('seasonInfo').innerHTML = response.season;
-        document.getElementById('episodeInfo').innerHTML = response.number;
-        document.getElementById('overview').innerHTML = response.overview;
-        document.getElementById('imdbHREF').href = "https://www.imdb.com/title/" + response.ids.imdb;
-        var element = document.getElementById("info");
+    if (this.readyState === 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      document.getElementById('title').innerHTML = response.title;
+      document.getElementById('runtime').innerHTML = response.runtime + ' mins.';
+      document.getElementById('rating').innerHTML = (response.rating).toFixed(1) + '/10';
+      document.getElementById('seasonInfo').innerHTML = response.season;
+      document.getElementById('episodeInfo').innerHTML = response.number;
+      document.getElementById('overview').innerHTML = response.overview;
+      document.getElementById('imdbHREF').href = "https://www.imdb.com/title/" + response.ids.imdb;
+      var element = document.getElementById("info");
 
-        if (element.classList.contains('hidden') == true) {
-          element.classList.toggle('hidden');
-          // remove loader
-        }
+      if (element.classList.contains('hidden') == true) {
+        element.classList.toggle('hidden');
+        // remove loader
       }
-    };
-    request.send();
+    }
+  };
+  request.send();
 }
 // Trakt API - Gets the information of the random episode.
 
@@ -178,20 +178,19 @@ function filterToggle() {
 
 //DISQUS
 function loadDisqus() {
-  var disqus_config = function () {
+  var disqus_config = () => {
     this.page.identifier = pageURL; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     console.log(this.page.identifier);
   };
 
-  (function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://randommifflin.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
+  (function () { // DON'T EDIT BELOW THIS LINE
+    var d = document,
+      s = d.createElement('script');
+    s.src = 'https://randommifflin.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
   })();
-};
-//DISQUS
-
+}; //DISQUS
 
 // function showDisqus(){
 //   var element = document.getElementById("disqus_thread");
@@ -203,16 +202,16 @@ function loadDisqus() {
 //     document.getElementById('loadComments').innerHTML = 'Show Comments';
 //   }
 // }
-  //Change Page Title // DONE
-function changeTitle(){
-  var pageTitle = 'Random Mifflin | ' + 'Season ' + seasonNo + ' Episode ' + episodeNo;
+//Change Page Title // DONE
+function changeTitle() {
+  var pageTitle = `Random Mifflin | Season ${seasonNo} Episode ${episodeNo}`;
   document.title = pageTitle;
   console.log(pageTitle);
 }
 //Change Page Title
 
 //change page URL
-function changeURL(){
+function changeURL() {
   history.pushState(null, '', '?' + 's=' + seasonNo + '&' + 'e=' + episodeNo);
   pageURL = '?' + 's=' + seasonNo + '&' + 'e=' + episodeNo;
 }
